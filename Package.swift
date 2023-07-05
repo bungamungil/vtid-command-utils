8090a60f7313d9d5a93972add7847cbed1cac403
Naufal Fachrian <naufal.fachrian@gmail.com>
2023-07-05 16:51:55 +0700

    Update library name

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "vtid-command-utils",
+            name: "VTIDCommandUtils",
             targets: ["VTIDCommandUtils"]
         ),
     ],
