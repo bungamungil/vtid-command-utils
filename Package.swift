c6c9b51b21ef4d5b4bf69134e6d92692a0276184
Naufal Fachrian <naufal.fachrian@gmail.com>
2023-07-06 10:50:52 +0700

    Add db to CommandConfig

--- Package.swift
+++ Package.swift
@@ -20,6 +20,7 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/vapor/console-kit.git", from: "4.6.0"),
         .package(url: "https://github.com/vapor/fluent-kit.git", from: "1.42.4"),
+        .package(url: "https://github.com/bungamungil/vtid-core.git", exact: "0.4.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -29,6 +30,7 @@ let package = Package(
             dependencies: [
                 .product(name: "ConsoleKit", package: "console-kit"),
                 .product(name: "FluentKit", package: "fluent-kit"),
+                .product(name: "VTIDCore", package: "vtid-core"),
             ]
         ),
     ]
--- Sources/VTIDCommandUtils/CommandConfig.swift
+++ Sources/VTIDCommandUtils/CommandConfig.swift
@@ -8,18 +8,33 @@
 import FluentKit
 import NIOCore
 import NIOPosix
+import VTIDCore
 
 
 public struct CommandConfig {
     
-    public var databases: Databases = Databases(
-        threadPool: NIOThreadPool(
-            numberOfThreads: System.coreCount
-        ),
-        on: MultiThreadedEventLoopGroup(
-            numberOfThreads: System.coreCount
+    public var databases: Databases
+    
+    public var db: Database
+    
+    init() {
+        self.databases = Databases(
+            threadPool: NIOThreadPool(
+                numberOfThreads: System.coreCount
+            ),
+            on: MultiThreadedEventLoopGroup(
+                numberOfThreads: System.coreCount
+            )
         )
-    )
+        self.databases.use(
+            VTIDCore.DatabaseConfig,
+            as: VTIDCore.DatabaseID
+        )
+        self.db = self.databases.database(
+            logger: .init(label: "vtid-db"),
+            on: self.databases.eventLoopGroup.any()
+        )!
+    }
     
 }
 
