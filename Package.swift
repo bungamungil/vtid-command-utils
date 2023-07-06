c54d45c51233dd9df2527b5b8b9b4cdb82513ef6
Naufal Fachrian <naufal.fachrian@gmail.com>
2023-07-06 11:06:24 +0700

    Update vtid-core

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/vapor/console-kit.git", from: "4.6.0"),
         .package(url: "https://github.com/vapor/fluent-kit.git", from: "1.42.4"),
-        .package(url: "https://github.com/bungamungil/vtid-core.git", exact: "0.4.0"),
+        .package(url: "https://github.com/bungamungil/vtid-core.git", exact: "0.4.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
